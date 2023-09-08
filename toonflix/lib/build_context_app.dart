5e3e3fa8250479f77352b642e295ed9ab8d70703
1Bronze <khdkkhdd@gmail.com>
2023-09-08 11:16:37 +0900

    [feat] buildcontext

---
+++ toonflix/lib/build_context_app.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class BuildContextApp extends StatefulWidget {
+  @override
+  State<BuildContextApp> createState() => _BuildContextAppState();
+}
+
+class _BuildContextAppState extends State<BuildContextApp> {
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyLargeTitle(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
+    );
+  }
+}
